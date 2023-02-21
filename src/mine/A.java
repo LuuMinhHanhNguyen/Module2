d95bfd5c3c421719dff417ec47b444f8d3840dd6
LuuMinhHanhNguyen <121532281+LuuMinhHanhNguyen@users.noreply.github.com>
2023-02-21 19:54:54 +0700

    Delete src/mine directory

--- src/mine/A.java
+++
@@ -1,9 +0,0 @@
-package mine;
-
-public class A {
-    static public class B {
-    }
-
-    public class C {
-    }
-}
--- src/mine/Controller.java
+++
@@ -1,9 +0,0 @@
-package mine;
-
-public class Controller {
-    public static void main(String[] args) {
-        A.B o = new A.B();
-      //  A.C oo = new A.C();
-        A.C ooo = new A().new C();
-    }
-}
