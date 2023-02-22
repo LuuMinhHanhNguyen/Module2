a264c27b7393c73509fa3502215d080a36cd415c
LuuMinhHanhNguyen <luuminhhanhnguyen@gmail.com>
2023-02-22 17:57:46 +0700

    ss

--- src/ss5_accessmodifier_static/circle/CheckCircle.java
+++
@@ -1,9 +0,0 @@
-package ss5_accessmodifier_static.circle;
-
-public class CheckCircle {
-    public static void main(String[] args) {
-        Circle circle = new Circle();
-        //circle.color = "green";
-       // System.out.println(circle.color);
-    }
-}
