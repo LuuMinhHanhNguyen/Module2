bd5cc86c7ef6ff4bf02eb710913c0a00b092704b
LuuMinhHanhNguyen <luuminhhanhnguyen@gmail.com>
2023-02-28 17:08:28 +0700

    ss10

---
+++ src/ss10_arrayList/mylist/MyListTest.java
@@ -0,0 +1,11 @@
+package ss10_arrayList.mylist;
+
+import java.util.ArrayList;
+
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList myList = new MyList(5);
+
+        System.out.println(myList);
+    }
+}
