9ccc1e96d5f4d31d292f91a097d34ee4f0590b3f
LuuMinhHanhNguyen <luuminhhanhnguyen@gmail.com>
2023-03-02 22:32:13 +0700

    ss

--- src/ss12_java_collection_framework/product_management/repository/IProductRepository.java
+++ src/ss12_java_collection_framework/product_management/repository/IProductRepository.java
@@ -1,4 +1,20 @@
 package ss12_java_collection_framework.product_management.repository;
 
+import ss12_java_collection_framework.product_management.model.Product;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public interface IProductRepository {
+    List<Product> displayAll();
+
+    void addProduct(Product newProduct);
+    void findProductByName(String productName);
+
+    void deleteProduct(int ID);
+
+    void editProduct(int ID);
+
+    void sortByPrice(boolean option);
+
 }
--- src/ss12_java_collection_framework/product_management/service/IProductService.java
+++ src/ss12_java_collection_framework/product_management/service/IProductService.java
@@ -1,4 +1,12 @@
 package ss12_java_collection_framework.product_management.service;
 
 public interface IProductService {
+    void displayAll();
+
+    void addProduct();
+
+    void findProductById();
+    void deleteProduct();
+    void editProduct();
+    void sortByPrice();
 }
