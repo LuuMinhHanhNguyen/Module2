dbd4f32e1bda46644a5eec35b91a104a9c45a6ef
LuuMinhHanhNguyen <luuminhhanhnguyen@gmail.com>
2023-03-03 19:15:24 +0700

    ss

--- src/ss13_searching_algorithm/BinarySearchByRecursion.java
+++ src/ss13_searching_algorithm/BinarySearchByRecursion.java
@@ -3,16 +3,25 @@ package ss13_searching_algorithm;
 public class BinarySearchByRecursion {
 
     public static void main(String[] args) {
-        int[] array = {1,3,5,7,9,11};
-        System.out.println(binarySearchByRecursion(array, 0, array.length -1, 9));
+        int[] array = {1, 3, 5, 7, 9, 11};
+        System.out.println(binarySearchByRecursion(array, 0, array.length - 1, 9));
     }
-     public static int binarySearchByRecursion(int[] arr, int start, int end, int key){
-         int mid = (start + end) / 2;
 
-             if(arr[mid] > key) return binarySearchByRecursion(arr, start, mid -1, key);
-             else if(arr[mid] < key) return binarySearchByRecursion(arr, mid + 1, end, key);
-             else if(arr[mid] == key) return mid;
+    public static int binarySearchByRecursion(int[] arr, int start, int end, int key) {
+        int mid = (start + end) / 2;
+        if (end >= start) {
+            if (key == arr[mid]) {
+                return mid;
+            }
 
-         return -1;
-     }
+            if (key > arr[mid]) {
+                return binarySearchByRecursion(arr, mid + 1, end, key);
+            }
+
+            if (key < arr[mid]) {
+                return binarySearchByRecursion(arr, start, mid - 1, key);
+            }
+        }
+        return -1;
+    }
 }
